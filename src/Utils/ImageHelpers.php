308da2ae3af85a95712cccd44fd36631ee3c8eb6
Hunman <sanyi.exe@gmail.com>
2019-06-23 20:25:21 +0200

    Final fixes

--- src/Utils/ImageHelpers.php
+++ src/Utils/ImageHelpers.php
@@ -14,16 +14,16 @@ namespace CharlotteDunois\Yasmin\Utils;
  */
 class ImageHelpers {
     /**
-     * Returns the default extension for an image
-     * @param  string  $image  The image hash.
+     * Returns the default extension for an image.
+     * @param string  $image  The image hash.
      * @return string  Returns "gif" if the hash begins with "a_", otherwise "png".
      */
     static function getImageExtension(string $image): string {
         return (\strpos($image, 'a_') === 0 ? 'gif' : 'png');
     }
-
+    
     /**
-     * Returns whether the input number is the power of 2
+     * Returns whether the input number is a power of 2.
      * @param int|null  $size
      * @return bool
      */
